46390660e66d0771de84fa2f631fc02513c94c4e
jesseharold <jesseharold@gmail.com>
2016-12-07 19:23:45 -0800

    add file read and write, not tested yet

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 var ClozeFlashcard = require("./ClozeFlashcard");
 var BasicFlashcard = require("./BasicFlashcard");
+var fs = require("fs");
 
-var newClozeCard = new ClozeFlashcard("Mama Jo is your mama.", 0, 7);
-var newBasicCard = new BasicFlashcard("Who is your mama?", "Mama Jo");
+var deck = [];
+var deckName = "myCards";
 
-console.log(newBasicCard.front);
-console.log(newBasicCard.back);
-console.log(newClozeCard.getPartialText());
+function makeBasicCard(front, back){
+    var newCard = new BasicFlashcard(front, back);
+    deck.push(newCard);
+}
+function makeClozeCard(text, startCloze, endCloze){
+    var newCard = new BasicFlashcard(text, startCloze, endCloze);
+    deck.push(newCard);
+}
+function saveDeck(deckName){
+    fs.writeFile(deckName + ".json", JSON.stringify(deck), function(error){
+        if(error){
+            return console.log(error);
+        }
+        console.log("We wrote to the file " + deckName + ".json");
+    });
+}
+function getDeck(deckName){
+    fs.readFile(deckName + ".json", function(error, data){
+        deck = JSON.parse(data);
+        console.log(deck);
+    });
+}
+
+//console.log(newBasicCard.front);
+//console.log(newBasicCard.back);
+//console.log(newClozeCard.getPartialText());
